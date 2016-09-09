dea4bf3310dd87d312bd1d0ad3557326fc5b2ded
Ivan <ivan.mladenov@abbaty.int>
2016-09-09 16:16:33 +0300

    Finish create action tests

---
+++ BlogSystem/BlogSystem.Tests/Web/BlogControllerTests/CreateTests.cs
@@ -0,0 +1,30 @@
+﻿using BlogSystem.Domain.Models;
+using BlogSystem.Web.Controllers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogSystem.Tests.Web.BlogControllerTests
+{
+    [TestFixture]
+    class CreateTests
+    {
+        [Test]
+        public void TestCreate_ShouldReturnNewPostAndCorrectView()
+        {
+            var controller = new BlogController(null);
+
+            var result = controller.Create();
+
+            var expectedPost = result.Model as Post;
+            var expectedViewName = result.ViewName;
+
+            Assert.IsNotNull(expectedPost);
+            Assert.IsInstanceOf<Post>(expectedPost);
+            Assert.AreEqual(expectedViewName, "Edit");
+        }
+    }
+}
