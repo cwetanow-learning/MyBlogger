c39b0d5c4e8c37ca27651744ab6cf0521bafb798
Ivan <ivan.mladenov@abbaty.int>
2016-09-10 16:12:34 +0300

    Move display submit button to new js file

---
+++ BlogSystem/BlogSystem.Web/Scripts/comment-textarea-methods.js
@@ -0,0 +1,14 @@
+﻿var done = function () {
+    $('.form-control').val('');
+    $('#submit').hide();
+};
+
+$('#submit').hide();
+
+$('.form-control').keyup(function () {
+    if ($(this).val().length > 0) {
+        $('#submit').show();
+    } else {
+        $('#submit').hide();
+    }
+});
