504a6471666170280f70b21334874b11f642ebae
Ivan <ivan.mladenov@abbaty.int>
2016-09-10 15:54:08 +0300

    Write test for CommentPost

--- BlogSystem/BlogSystem.Tests/Web/CommentControllerTests/TestCommentPost.cs
+++ BlogSystem/BlogSystem.Tests/Web/CommentControllerTests/TestCommentPost.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using BlogSystem.Domain.Contracts;
+using BlogSystem.Domain.Models;
+using BlogSystem.Web.Controllers;
+using BlogSystem.Web.Models;
+using Moq;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +12,21 @@ using System.Threading.Tasks;
 
 namespace BlogSystem.Tests.Web.CommentControllerTests
 {
+    [TestFixture]
     public class TestCommentPost
     {
+        [Test]
+        public void TestCommentPost_PassValidData_ShouldCallRepositoryWriteComment()
+        {
+            var mockedModel = new Mock<CommentViewModel>();
+
+            var mockedRepository = new Mock<ICommentRepository>();
+
+            var controller = new CommentController(mockedRepository.Object);
+
+            controller.CommentPost(mockedModel.Object);
+
+            mockedRepository.Verify(x => x.WriteComment(It.IsAny<Comment>(), It.IsAny<int>(), It.IsAny<string>()), Times.Once);
+        }
     }
 }
