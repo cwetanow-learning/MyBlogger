f34c9fbfda2520d57193bf733851f8f69524f535
Ivan <ivan.mladenov@abbaty.int>
2016-09-12 15:56:19 +0300

    Write more admin actions

--- BlogSystem/BlogSystem.Web/Areas/Administration/Controllers/AdminController.cs
+++ BlogSystem/BlogSystem.Web/Areas/Administration/Controllers/AdminController.cs
@@ -1,4 +1,10 @@
-﻿using BlogSystem.Web.Controllers.Abstract;
+﻿using BlogSystem.Domain.Contracts;
+using BlogSystem.Domain.Models;
+using BlogSystem.Domain.Utils;
+using BlogSystem.Web.Controllers.Abstract;
+using Microsoft.AspNet.Identity;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -8,11 +14,36 @@ namespace BlogSystem.Web.Areas.Administration.Controllers
     [RouteArea]
     public class AdminController : BaseController
     {
+        private IPostRepository postRepository;
+        private ICommentRepository commentRepository;
+
+        public AdminController(IPostRepository postRepo, ICommentRepository commentRepo)
+        {
+            this.postRepository = postRepo;
+            this.commentRepository = commentRepo;
+        }
+
         public ActionResult Index()
         {
-            var users = this.UserManager.Users.ToList();
+            var currentDate = DateHelper.GetCurrentTime();
+
+            var users = this.UserManager.Users
+                .ToList();
+            users = users
+                .Where(u => !this.UserManager.IsLockedOut(u.Id))
+                .ToList();
 
             return View(users);
+        }
+
+        public ActionResult Delete(string id)
+        {
+            var user = this.UserManager.FindById(id);
+            user.LockoutEndDateUtc = DateHelper.GetCurrentTime().AddYears(200);
+
+            this.UserManager.Update(user);
+
+            return this.RedirectToAction("Index");
         }
     }
 }
