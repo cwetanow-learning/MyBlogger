6825d3bd5eafb60df940a2e6aa23ecf1d05f3024
Ivan <ivan.mladenov@abbaty.int>
2016-09-12 12:49:38 +0300

    Write tests for post controller list action

---
+++ BlogSystem/BlogSystem.Tests/Web/CommentControllerTests/DeleteTests.cs
@@ -0,0 +1,29 @@
+﻿using BlogSystem.Domain.Contracts;
+using BlogSystem.Web.Controllers;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogSystem.Tests.Web.CommentControllerTests
+{
+    [TestFixture]
+    class DeleteTests
+    {
+        [TestCase(1,2)]
+        [TestCase(2,11)]
+        public void TestDelete_ShouldCallDeleteFromRepository(int id, int commentId)
+        {
+            var mockedRepository = new Mock<ICommentRepository>();
+
+            var controller = new CommentController(mockedRepository.Object);
+
+            controller.Delete(commentId, id);
+
+            mockedRepository.Verify(x => x.DeleteComment(commentId), Times.Once);
+        }
+    }
+}
---
+++ BlogSystem/BlogSystem.Tests/Web/PostControllerTests/ListTests.cs
@@ -0,0 +1,29 @@
+﻿using BlogSystem.Web.Controllers;
+using BlogSystem.Web.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogSystem.Tests.Web.PostControllerTests
+{
+    [TestFixture]
+    class ListTests
+    {
+        [TestCase(true, true)]
+        [TestCase(false, true)]
+        [TestCase(false, false)]
+        [TestCase(true, false)]
+        public void TestList_ShouldReturnModelWithCorrectProperties(bool date, bool rating)
+        {
+            var controller = new PostController(null);
+
+            var result = controller.List(date, rating).Model as ListPostViewModel;
+
+            Assert.AreEqual(date, result.ByDate);
+            Assert.AreEqual(rating, result.ByRating);
+        }
+    }
+}
