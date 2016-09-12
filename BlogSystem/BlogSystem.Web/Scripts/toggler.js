84040e93e6e10e4868603980a23817f07d4e3216
Ivan <ivan.mladenov@abbaty.int>
2016-09-12 13:28:15 +0300

    Write script to animate fading

---
+++ BlogSystem/BlogSystem.Web/Scripts/toggler.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $('#posts').hide();
+    $('#comments').hide();
+});
+
+$('#postsToggle').click(function () {
+    $('#posts').fadeToggle();
+});
+
+$('#commentsToggle').click(function () {
+    $('#comments').fadeToggle();
+});
