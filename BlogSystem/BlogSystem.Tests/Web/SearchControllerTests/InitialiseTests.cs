cc5e6af67af5dfe1460f8970ef47f5856b7cc4ed
Ivan <ivan.mladenov@abbaty.int>
2016-09-13 13:47:23 +0300

    Write tests for search controller

---
+++ BlogSystem/BlogSystem.Tests/Web/SearchControllerTests/InitialiseTests.cs
@@ -0,0 +1,37 @@
+﻿using BlogSystem.Domain.Contracts;
+using BlogSystem.Web.Controllers;
+using BlogSystem.Web.Controllers.Abstract;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogSystem.Tests.Web.SearchControllerTests
+{
+    [TestFixture]
+    class InitialiseTests
+    {
+        [Test]
+        public void ConstructorTests_ShouldInitialiseCorrectInstanceOfBase()
+        {
+            var mockedPostRepository = new Mock<IPostRepository>();
+
+            var controller = new SearchController(mockedPostRepository.Object);
+
+            Assert.IsInstanceOf<BaseController>(controller);
+        }
+
+        [Test]
+        public void ConstructorTests_ShouldInitialiseCorrectly()
+        {
+            var mockedPostRepository = new Mock<IPostRepository>();
+
+            var controller = new SearchController(mockedPostRepository.Object);
+
+            Assert.IsNotNull(controller);
+        }
+    }
+}
