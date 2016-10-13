4848be610a1485e3c1ea1671d771f07de54a1749
Ivan <madritistaaa@gmail.com>
2016-10-13 19:06:02 +0300

    Write constructor tests

---
+++ BlogSystem/BlogSystem.Tests/DomainTests/RepositoryTests/ConstructorTests.cs
@@ -0,0 +1,33 @@
+﻿using BlogSystem.Domain.Concrete;
+using BlogSystem.Domain.Contracts;
+using Moq;
+using NUnit.Framework;
+
+namespace BlogSystem.Tests.DomainTests.RepositoryTests
+{
+    [TestFixture]
+    class ConstructorTests
+    {
+        [Test]
+        public void TestPostRepositoryConstructor_ShouldInitialiseCorrectly()
+        {
+            var mockedDbContext = new Mock<IApplicationDbContext>();
+
+            var repository = new PostRepository(mockedDbContext.Object);
+
+            Assert.IsNotNull(repository);
+            Assert.IsInstanceOf<IPostRepository>(repository);
+        }
+
+        [Test]
+        public void TestCommentRepositoryConstructor_ShouldInitialiseCorrectly()
+        {
+            var mockedDbContext = new Mock<IApplicationDbContext>();
+
+            var repository = new CommentRepository(mockedDbContext.Object);
+
+            Assert.IsNotNull(repository);
+            Assert.IsInstanceOf<ICommentRepository>(repository);
+        }
+    }
+}
--- BlogSystem/BlogSystem.Web/Infrastructure/NinjectDependencyResolver.cs
+++ BlogSystem/BlogSystem.Web/Infrastructure/NinjectDependencyResolver.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using BlogSystem.Domain.Concrete;
 using System.Web.Mvc;
+using BlogSystem.Domain.Utils;
 
 namespace BlogSystem.Web.Infrastructure
 {
@@ -28,7 +29,7 @@ namespace BlogSystem.Web.Infrastructure
         private void AddBindings()
         {
             this.kernel.Bind<IApplicationDbContext>().To<ApplicationDbContext>();
-
+            this.kernel.Bind<IDateProvider>().To<DateProvider>();
             this.kernel.Bind<IPostRepository>().To<PostRepository>();
             this.kernel.Bind<ICommentRepository>().To<CommentRepository>();
         }
